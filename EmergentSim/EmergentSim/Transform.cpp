bb01fc2181767fb716bc6cede64522bb421fa606
結衣 <gressek13@gmail.com>
2021-05-08 23:17:57 -0700

    Add forward functionality in transform

--- EmergentSim/EmergentSim/Transform.cpp
+++ EmergentSim/EmergentSim/Transform.cpp
@@ -6,5 +6,19 @@ Transform::Transform(int x, int y, Direction dir) : m_x(x), m_y(y), m_dir(dir)
 
 void Transform::Forward(int count)
 {
-
+	switch (m_dir)
+	{
+	case Direction::UP:
+		m_y += count;
+		break;
+	case Direction::DOWN:
+		m_y -= count;
+		break;
+	case Direction::RIGHT:
+		m_x += count;
+		break;
+	case Direction::LEFT:
+		m_x -= count;
+		break;
+	}
 }
