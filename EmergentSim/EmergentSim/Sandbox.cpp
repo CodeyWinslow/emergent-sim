006f264c10d0192213719b350ebf1703eab7e755
結衣 <gressek13@gmail.com>
2021-05-08 20:56:18 -0700

    Get rid of unneeded comments Remove VisitAll function comment from Sandbox.cpp

--- EmergentSim/EmergentSim/Sandbox.cpp
+++ EmergentSim/EmergentSim/Sandbox.cpp
@@ -8,14 +8,3 @@ Sandbox::~Sandbox()
 	m_width = 0;
 	m_height = 0;
 }
-
-/*void Sandbox::VisitAll(void (*Visit)(Entity* entity))
-{
-	for (int i = 0; i < m_width; ++i)
-	{
-		for (int j = 0; j < m_height; ++j)
-		{
-			Visit(m_sandbox[i][j]);
-		}
-	}
-}*/
